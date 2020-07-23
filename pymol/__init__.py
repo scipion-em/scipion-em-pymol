0e167838200bfb12ba37adc81274a1845d738316
DavidHerreros <david.herreroscalero@gmail.com>
2020-07-23 10:16:41 +0200

    Improved installation from GitHub

--- pymol/__init__.py
+++ pymol/__init__.py
@@ -24,7 +24,12 @@
 # *
 # **************************************************************************
 
+
+import os
+
 import pwem
+from scipion.install.funcs import VOID_TGZ
+
 from .constants import PYMOL_HOME, V2_4_0
 
 _logo = "icon.png"
@@ -51,29 +56,33 @@ class Plugin(pwem.Plugin):
     @classmethod
     def runPymolScript(cls, protocol, script):
         pymolCall = '%s %s && %s/pymol-build/bin/pymol -cq' % (cls.getCondaActivationCmd(),
-                                                               cls.getPymolActivation(), cls.getHome())
+                                                               cls.getPymolActivation(),
+                                                               os.path.join(cls.getHome(), 'pymol'))
         protocol.runJob(pymolCall, script)
 
     @classmethod
     def defineBinaries(cls, env):
         SW_EM = env.getEmFolder()
 
+        pymol240_commands = []
+        pymol240_commands.append(('wget -c https://github.com/schrodinger/pymol-open-source/archive/95a44ad.tar.gz', "95a44ad.tar.gz"))
+        pymol240_commands.append(("tar -xvf 95a44ad.tar.gz", []))
+        pymol240_commands.append(("mv pymol-open-source-* pymol", []))
+
         installationCmd = cls.getCondaActivationCmd()
         installationCmd += 'conda create -y -n pymol%s python=3.7 && ' % V2_4_0
         installationCmd += 'conda activate pymol%s && ' % V2_4_0
         installationCmd += 'conda install -c anaconda pyqt -y && conda install -c conda-forge pmw -y && '
         installationCmd += 'conda install -c openbabel openbabel -y && '
-        installationCmd += 'cd .. && wget -N https://github.com/rcsb/mmtf-cpp/archive/7c74b18.tar.gz && '
-        installationCmd += 'tar -xf 7c74b18.tar.gz && mv pymol-open-source-95a44ada8bacfe345905a5df0a70376eb1b455b1 pymol && '
+        installationCmd += 'wget -N https://github.com/rcsb/mmtf-cpp/archive/7c74b18.tar.gz && '
+        installationCmd += 'tar -xf 7c74b18.tar.gz && '
         installationCmd += 'mv mmtf-cpp*/include/mmtf* pymol/include/ && '
-        installationCmd += 'cd pymol && python setup.py build install --home=%s/pymol/pymol-build && ' % SW_EM
+        installationCmd += 'cd pymol && python setup.py build install --home=%s/pymol-%s/pymol/pymol-build && ' % (SW_EM, V2_4_0)
         installationCmd += 'touch pymol_installed'
-        pymol240_commands = [(installationCmd, "%s/pymol/pymol_installed" % SW_EM)]
+        pymol240_commands.append((installationCmd, "%s/pymol-%s/pymol/pymol_installed" % (SW_EM, V2_4_0)))
 
 
         env.addPackage('pymol', version=V2_4_0,
-                       url='https://github.com/schrodinger/pymol-open-source/archive/95a44ad.tar.gz',
-                       buildDir='pymol-open-source-95a44ada8bacfe345905a5df0a70376eb1b455b1',
+                       tar=VOID_TGZ,
                        commands=pymol240_commands,
-                       targetDir='pymol',
                        default=True)
